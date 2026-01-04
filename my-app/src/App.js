7b7ade834be162b4ac8579559bdac2c136d50a32
JyP-rodriguez <teamwawawa16@gmail.com>
2026-01-03 22:34:43 -0400

    Update DEV ENVIROTMENT

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Eilyn Gutierrez.
+          WELCOME TO MY DEV ENVIROtMENT.
         </p>
         <a
           className="App-link"
